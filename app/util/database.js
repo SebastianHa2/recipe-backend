45ea7b1f0aacee1841b143fb6893783fbaa970f0
Sebastian <hacza212@gmail.com>
2021-03-31 12:08:13 +0100

    connected to mysql db on heroku

--- app/util/database.js
+++ app/util/database.js
@@ -3,9 +3,9 @@ const Sequelize = require('sequelize').Sequelize
 // new Sequelize('database_name', 'mysql_user', 'mysql_password')
 
 // dialect and host are optional
-const sequelize = new Sequelize('recipeDB', 'Sebastian', 'Bananowy', {
+const sequelize = new Sequelize('heroku_8915a890696307d', 'b8fd580a23f692', '11b61a78', {
     dialect: 'mysql',
-    host: 'localhost'
+    host: 'eu-cdbr-west-01.cleardb.com'
 })
 
 module.exports = sequelize
